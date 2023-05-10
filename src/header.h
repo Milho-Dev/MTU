ff2c58b177fc4f6bca750455098b2d19ef11bd40
Emilio Bottoni <emilio.bottoni@edge.ufal.br>
2023-05-10 08:07:35 -0300

    Some optimization and fixes

--- src/header.h
+++ src/header.h
@@ -8,11 +8,21 @@
 #include <SNMP_Agent.h>
 #include <DHTesp.h>
 
-#define WIFI_SSID "SSID"
-#define WIFI_PSWD "PASSWORD"
+#define WIFI_SSID "MilhoRasp"
+#define WIFI_PSWD "MilhoNerfado"
 
-#define TEMPERATURE_OID ".1.3.6.1.4.1.1234.1.0"
+#define STATIC_IP 10, 42, 0, 200
+#define GATEWAY 10, 42, 0, 1
+#define SUBNET 255, 255, 255, 0
+#define PRIMARYDNS 8, 8, 8, 8
+#define SECONDARYDNS 8, 8, 4, 4
+
+
+#define SNMP_COMMUNITY "server_ROOM"
+
+#define TEMPERATURE_OID ".1.3.6.1.4.1.1234.1.2"
 #define HUMIDITY_OID ".1.3.6.1.4.1.1234.1.1"
+#define DHT22_STATE ".1.3.6.1.4.1.1234.1.0"
 #define TIMESTAMP_OID ".1.3.6.1.2.1.1.3.0"
 
 #define LED_GPIO_PIN GPIO_NUM_22
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,8 @@ TaskHandle_t taskHandle;
 int temperature = 0;
 int humidity = 0;
 
+int dht22_state = DHTesp::ERROR_NONE;
+
 /****** WIFI stuff *****/
 
 const char *ssid = WIFI_SSID;
@@ -16,12 +18,23 @@ const char *pswd = WIFI_PSWD;
 
 WiFiUDP udp;
 
+// Set your Static IP address
+IPAddress local_IP(STATIC_IP);
+// Set your Gateway IP address
+IPAddress gateway(GATEWAY);
+
+IPAddress subnet(SUBNET);
+IPAddress primaryDNS(PRIMARYDNS);     // optional
+IPAddress secondaryDNS(SECONDARYDNS); // optional
+
 /***** SNMP stuff ******/
 
-SNMPAgent snmp = SNMPAgent("public");
+SNMPAgent snmp = SNMPAgent(SNMP_COMMUNITY);
 
 ValueCallback *temperatureOID;
 ValueCallback *humidityOID;
+
+ValueCallback *dht22_stateOID;
 
 TimestampCallback *timestampCallbackOID;
 uint32_t tensOfMillisCounter = 0;
@@ -43,17 +56,24 @@ void setup()
         NULL,              /* Task input parameter */
         tskIDLE_PRIORITY,  /* Priority of the task */
         &taskHandle,       /* Task handle. */
-        1);
+        0);
 
-    // Connect WiFI
+    // Configures static IP address
+    if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
+    {
+        Serial.println("STA Failed to configure");
+    }
+
     WiFi.begin(ssid, pswd);
-
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
+        Serial.print(".");
     }
-
-    Serial.print("Connected, Got IP:");
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
 
     // Setup SNMP pre-requirements
@@ -63,6 +83,8 @@ void setup()
     // Buffer registering for SNMP
     temperatureOID = snmp.addIntegerHandler(TEMPERATURE_OID, &temperature);
     humidityOID = snmp.addIntegerHandler(HUMIDITY_OID, &humidity);
+
+    dht22_stateOID = snmp.addIntegerHandler(DHT22_STATE, &dht22_state);
 
     timestampCallbackOID = (TimestampCallback *)snmp.addTimestampHandler(TIMESTAMP_OID, &tensOfMillisCounter);
 
@@ -80,14 +102,19 @@ void loop()
 // Read DHT22
 void readSensor(void *pvParameters)
 {
+    DHTesp::DHT_ERROR_t err;
     while (true)
     {
         delay(dht.getMinimumSamplingPeriod());
         Serial.println("READING");
 
-        humidity = (dht.getHumidity() * 10);
-        temperature = (dht.getTemperature() * 10);
+        err = dht.getStatus();
 
+        humidity = err == DHTesp::ERROR_NONE ? (dht.getHumidity() * 10) : 0;
+        temperature = err == DHTesp::ERROR_NONE ? (dht.getTemperature() * 10) : 0;
+        dht22_state = err;
+
+        Serial.printf("Sensor STATE: %s\n", dht22_state == DHTesp::ERROR_NONE ? "OK" : "FAIL");
         Serial.printf("Humidity: %i\n", humidity);
         Serial.printf("Temperature: %i\n", temperature);
     }
